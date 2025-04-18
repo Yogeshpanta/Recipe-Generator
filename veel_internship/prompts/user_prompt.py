0411eb52e561098b4e82219e4947ae130eeefdc0
yogeshpanta <yogeshpanta33@gmail.com>
2025-04-18 15:06:12 +0545

    modified user prompt

--- veel_internship/prompts/user_prompt.py
+++ veel_internship/prompts/user_prompt.py
@@ -2,10 +2,20 @@ from schemas.userinput import GiveRecipeInput
 from schemas.pydantic_schema import RequestRecipe
 from schemas.userinput import GiveRecipeInput
 
-c1 = GiveRecipeInput(foodtype=None, summary=None, ingridients=None)
-c1.give_input()
+# c1 = GiveRecipeInput(foodtype=None, summary=None, ingridients=None)
+# c1.give_input()
 
-c2 = RequestRecipe(foodtype=c1.foodtype, summary=c1.summary, ingridients=c1.ingridients)
+# c2 = RequestRecipe(foodtype=c1.foodtype, summary=c1.summary, ingridients=c1.ingridients)
+
+# class USERPROMPT:
+#     """prompt that user gives """
+#     userprompt = (
+#         f"foodtype: {c2.foodtype}\n"
+#         f"summary: {c2.summary}\n"
+#         f"ingridients:{','.join(c2.ingridients)}"
+#     )
+
+c2 = RequestRecipe(foodtype="veg", summary="potato soup", ingridients=["potato", "onion", "chilly"])
 
 class USERPROMPT:
     """prompt that user gives """
@@ -14,3 +24,4 @@ class USERPROMPT:
         f"summary: {c2.summary}\n"
         f"ingridients:{','.join(c2.ingridients)}"
     )
+
