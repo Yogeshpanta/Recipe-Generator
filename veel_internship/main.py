62a3c2d430d9d60abf1e1e9bc9a7b968ffe6d43f
yogeshpanta <yogeshpanta33@gmail.com>
2025-04-18 15:04:08 +0545

    modified main.py

--- veel_internship/main.py
+++ veel_internship/main.py
@@ -1,6 +1,7 @@
-import logging
-from veel_internship.configs.logging_config import setup_logging
-from models.ollama_model import OllamaModel
+# import logging
+# from veel_internship.configs.logging_config import setup_logging
+from veel_internship.models.ollama_model import OllamaModel
+from veel_internship.prompts.prompt_templates import SYSTEMPROMPT
 
 # setup_logging()
 # logger = logging.getLogger(__name__)
@@ -14,8 +15,11 @@ if __name__ == "__main__":
     # logger.info("This is an info message.")
     # logger.warning("This is a warning message.")
     # logger.error("This is an error message.")
+    model = "qwen"
+    prompt = SYSTEMPROMPT.OLLAMA_PROMPT
+    temp = 0.5
 
-    obj1 = OllamaModel(model=None, format=None, prompt=None, temp=None)
+    obj1 = OllamaModel(model=model, prompt=prompt, temp=temp)
     print(obj1.ask_ollama())
 
 
