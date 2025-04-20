55e65fcc18dc5a3e93a67e59c920ee6cb4ee9157
yogeshpanta <yogeshpanta33@gmail.com>
2025-04-20 11:58:06 +0545

    userinput updated

--- veel_internship/schemas/userinput.py
+++ veel_internship/schemas/userinput.py
@@ -1,15 +1,30 @@
+import logging
+import coloredlogs
+
+
+def setup_logging(level="INFO"):
+    """Set up basic colored logging."""
+    logger = logging.getLogger()
+    coloredlogs.install(
+        level=level,
+        logger=logger,
+        fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    )
+
+
+setup_logging()
+
+
 class GiveRecipeInput:
     """Class to give input while requesting for recipe"""
+
     def __init__(self, foodtype, summary, ingridients):
         self.foodtype = foodtype
         self.summary = summary
         self.ingridients = ingridients
-    
-    # def __str__(self):
-    #     return "Making an input module for recipe generator"
 
     def give_input(self):
-        """ Gives input like foodtype, summary and ingridients"""
+        """Gives input like foodtype, summary and ingridients"""
         print("Give the food type (veg or non_veg)")
         while True:
             self.foodtype = input("Enter foodtype").lower().strip()
@@ -18,8 +33,8 @@ class GiveRecipeInput:
             else:
                 print("Please enter the veg or non_veg")
 
-        self.summary = input('Enter the summary of the Item').lower().strip()
+        self.summary = input("Enter the summary of the Item").lower().strip()
 
         ing = input(" Enter the ingridients separated by comma")
-        self.ingridients = [i.lower().strip() for i in ing.split(',')]
+        self.ingridients = [i.lower().strip() for i in ing.split(",")]
         return self.foodtype, self.summary, self.ingridients
