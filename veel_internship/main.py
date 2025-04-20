2915b61d0893a9e10e45279ebfe47f50e02f7bd9
yogeshpanta <yogeshpanta33@gmail.com>
2025-04-20 11:54:47 +0545

    main file updated

--- veel_internship/main.py
+++ veel_internship/main.py
@@ -1,25 +1,26 @@
-# import logging
-# from veel_internship.configs.logging_config import setup_logging
 from veel_internship.models.ollama_model import OllamaModel
 from veel_internship.prompts.prompt_templates import SYSTEMPROMPT
-
-# setup_logging()
-# logger = logging.getLogger(__name__)
-#     # Example usage of logging
-    
+import logging
+import coloredlogs
 
 
+def setup_logging(level="INFO"):
+    """Set up basic colored logging."""
+    logger = logging.getLogger()
+    coloredlogs.install(
+        level=level,
+        logger=logger,
+        fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    )
+
+
+setup_logging()
 
 
 if __name__ == "__main__":
-    # logger.info("This is an info message.")
-    # logger.warning("This is a warning message.")
-    # logger.error("This is an error message.")
     model = "qwen"
     prompt = SYSTEMPROMPT.OLLAMA_PROMPT
     temp = 0.5
-
+    logging.info("running main.py file")
     obj1 = OllamaModel(model=model, prompt=prompt, temp=temp)
     print(obj1.ask_ollama())
-
-
