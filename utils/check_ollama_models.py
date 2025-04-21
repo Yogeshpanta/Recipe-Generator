af249b3c2aec9345ee572cc057b6a184789468ab
yogeshpanta <yogeshpanta33@gmail.com>
2025-04-21 12:43:47 +0545

    added a function to check models

---
+++ utils/check_ollama_models.py
@@ -0,0 +1,8 @@
+import ollama
+def check_ollama_model():
+    """checks the installed model in system"""
+    ollama_model_list = []
+    response = ollama.list()["models"]
+    for model in response:
+        ollama_model_list.append(model["model"].split(":")[0])
+    return ollama_model_list
