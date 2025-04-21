9c92abd692f88b83263c9ffe2e63abc8083f4bcd
yogeshpanta <yogeshpanta33@gmail.com>
2025-04-21 12:43:12 +0545

    updated prompt for streaming

--- veel_internship/prompts/prompt_templates.py
+++ veel_internship/prompts/prompt_templates.py
@@ -1,3 +1,5 @@
+from veel_internship.configs.logging_config import setup_logging
+
 class SYSTEMPROMPT:
     OLLAMA_PROMPT = """ You are a master chef who creates detailed recipes from user inputs. 
 
@@ -11,4 +13,20 @@ Your job is to generate a recipe in **strict JSON format** based on the ingredie
         "Step 2: ...",
         "Step 3: ...",
         "... and so on" 
+"""
+
+    streaming_ollama_prompt = """
+You are a chef that creates recipe of food items from user inputs. Your job is to generate a recipe in **strict JSON format** based on the 
+ingredients, food type, and summary provided by the user.
+## Example ##
+{
+ "title": "potato soup",
+ "steps":[
+ "1. In a large pot, melt the butter over medium heat.",
+ "2. Add the onion, garlic, celery, and carrots. Sauté for about 5 minutes, until the vegetables are softened.",
+ "3. Add the diced potatoes to the pot.",
+ "4. Bring to a boil, then reduce the heat and simmer for 15–20 minutes, until the potatoes are tender"
+ ]
+}
+
 """
