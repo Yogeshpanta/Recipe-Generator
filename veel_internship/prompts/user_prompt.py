e338133f4e44bd24eb83dc729f0966dc88918f09
yogeshpanta <yogeshpanta33@gmail.com>
2025-04-29 13:21:25 +0545

    update user prompt with doc string

--- veel_internship/prompts/user_prompt.py
+++ veel_internship/prompts/user_prompt.py
@@ -1,17 +1,5 @@
 from schemas.pydantic_schema import RequestRecipe
-
-# c1 = GiveRecipeInput(foodtype=None, summary=None, ingridients=None)
-# c1.give_input()
-
-# c2 = RequestRecipe(foodtype=c1.foodtype, summary=c1.summary, ingridients=c1.ingridients)
-
-# class USERPROMPT:
-#     """prompt that user gives """
-#     userprompt = (
-#         f"foodtype: {c2.foodtype}\n"
-#         f"summary: {c2.summary}\n"
-#         f"ingridients:{','.join(c2.ingridients)}"
-#     )
+from veel_internship.configs.logging_config import setup_logging
 
 c2 = RequestRecipe(
     foodtype="veg", summary="potato soup", ingridients=["potato", "onion", "chilly"]
